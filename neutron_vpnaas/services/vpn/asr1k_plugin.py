04c530cba02a0adfce21a9ea24534a04f1fcc511
Sebastian Lohff <sebastian.lohff@sap.com>
2025-11-11 11:52:29 +0100

    Add custom asr1k-vpnaas extension
    For our asr1k driver we have extra supported extensions
    (vpn-asr1k-int-tunnel-ips for internal tunnel ips and
    vpn-asr1k-nat-address for tunnel nat addresses). As we don't want to
    override this in the default driver and we also don't know of another
    way to override the supported_extension_aliases we now have our own
    plugin.
    
    Originally we planned on keeping the plugin in our asr1k driver repo,
    but this did not work, as it couldn't find the all vpn/vpnaas
    extensions. The reason for this is that neutron loads extensions for all
    service plugins by looking at the base module of each service plugin and
    then using this to search them. If we keep our plugin in neutron-vpnaas,
    everything can be found. The other alternative would be to set
    api_extensions_path and set this to neutron-vpnaas. Details can be found
    in neutron.api.extensions.py get_extensions_path().

---
+++ neutron_vpnaas/services/vpn/asr1k_plugin.py
@@ -0,0 +1,29 @@
+# Copyright 2025 SAP SE
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron_vpnaas.services.vpn.plugin import VPNDriverPlugin
+
+
+class VPNASR1KDriverPlugin(VPNDriverPlugin):
+    supported_extension_aliases = [
+        "vpnaas",
+        "vpn-endpoint-groups",
+        "service-type",
+        "vpn-flavors",
+        "vpn-aes-ctr",
+        "vpn-asr1k-int-tunnel-ips",
+        "vpn-asr1k-nat-address",
+    ]
